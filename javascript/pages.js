bccf8f3ec513a339991ecd0d7225d5c90cb9bc85
tfotr241290 <franco_9024@hotmail.com>
2023-04-04 00:04:02 -0500

    checkout button verification

--- javascript/pages.js
+++ javascript/pages.js
@@ -1,5 +1,4 @@
 
-
 const home = `
 <div id="root">
     <header class="heading"></header>
@@ -86,14 +85,39 @@ const cars = `
     <div class="carList">
 
     </div>
+    <button type="submit" onclick="getPageContent('checkout')">CHECKOUT</button>
 `
+
+const checkout = `
+<div class="sHeading">
+<header class="sHeader">
+    <section class="logo"><a href="./index.html"><img class="nav-img" src="https://ik.imagekit.io/tfotr241290/Black_n_White_Luxury__Rent_Car_Logo.png?updatedAt=1679597309897" alt="navimgcar"></a></section><section class="media"><a href="https://www.facebook.com"><img class="face" src="https://cdn-icons-png.flaticon.com/128/3670/3670124.png" alt"facebooklogo"></a> <a href="https://www.twitter.com"><img class="twitter" src="https://cdn-icons-png.flaticon.com/128/3670/3670211.png" alt"facebooklogo"></a> <a href="https://www.instagram.com"><img class="insta" src="https://cdn-icons-png.flaticon.com/128/3670/3670125.png" alt"facebooklogo"></a></section>
+</header>
+</div>
+    <main>
+        <form action="" class="checkout-form">
+            <fieldset class="contact-form">
+                <legend>Contact Info</legend>
+                <label class="top" for="fname">First name* <input type="text" name="fname" id="fname" required></label>
+                <label class="top" for="email">Email* <input type="email" name="email" id="email" placeholder="example@gmail.com" required></label>
+                <label class="top" for="phone">Phone-number* <input type="tel" name="phone" id="phone" required></label>
+            </fieldset>
+            <fieldset class="payment-form">
+                <legend>Payment Info</legend>
+                <label class="top" for="cc-number">Card number<input id="cc-number" name="cc-number" autocomplete="cc-number" inputmode="numeric" maxlength="50" pattern="[\d ]{10,30}" required></label>
+                <label class="top" for="cc-name">Name on card<input id="cc-name" name="cc-name" autocomplete="cc-name" maxlength="50" pattern="[\p{L} \-\.]+" required></label>
+                <label id="cc-exp-csc" for="cc-exp">Expiry date<input id="cc-exp" name="cc-exp" autocomplete="cc-exp" placeholder="MM/YY" maxlength="5" required></label>
+                <label for="cc-csc">Security code<input id="cc-csc" name="cc-csc" inputmode="numeric" maxlength="3" required></label>
+            </fieldset>
+        </form>
+    </main>`
 document.querySelector(".carList")
 
 const pages = {
     'home':`${home}`,
     'cars':`${cars}`,
     'subscribe':`${subscribe}`,
-    'checkout':``
+    'checkout':`${checkout}`
 }
 
 function getPageContent(page){
